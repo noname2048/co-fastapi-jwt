cc37cd280158f18990858a6b73d2f6d64c6c8c71
Sungwook Choi <sungwook.csw@gmail.com>
2023-06-30 22:51:01 +0900

    feat: conftest testing

--- app/tests/apis/v1/test_user.py
+++ app/tests/apis/v1/test_user.py
@@ -4,29 +4,29 @@ from app.models.user import User
 from app.services import user as user_service
 
 
-def test_user_create(db: Session):
+def test_user_create(session: Session):
     email = "test@test.com"
     password = "test1234"
     user = user_service.create_user(
-        db,
+        session,
         email=email,
         plain_password=password,
     )
 
-    db_user = db.query(User).filter(User.email == email).one()
+    db_user = session.query(User).filter(User.email == email).one()
     assert db_user
     assert db_user.email == user.email == email
 
 
-def test_user_second(db: Session):
+def test_user_second(session: Session):
     email = "test@test.com"
     password = "test1234"
     user = user_service.create_user(
-        db,
+        session,
         email=email,
         plain_password=password,
     )
 
-    db_user = db.query(User).filter(User.email == email).one()
+    db_user = session.query(User).filter(User.email == email).one()
     assert db_user
     assert db_user.email == user.email == email
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,15 +1,16 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from app.db.base import Base
 from app.db.session import get_db
 from app.main import app
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/test"
+TEST_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/test"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    TEST_DATABASE_URL,
 )
 TestingSessionLocal = sessionmaker(
     autocommit=False,
@@ -33,11 +34,30 @@ client = TestClient(app)
 
 @pytest.fixture(autouse=True, scope="session")
 def db():
+    # assert TEST_DATABASE_URL.endswith("test")
+    # if database_exists(TEST_DATABASE_URL):
+    #     drop_database(TEST_DATABASE_URL)
+    # create_database(TEST_DATABASE_URL)
+
     try:  # TODO: change logic BETA for Change if Table exists
-        Base.metadata.drop_all(bind=engine)
         Base.metadata.create_all(bind=engine)
         test_db = TestingSessionLocal()
         yield test_db
     finally:
         test_db.close()
         Base.metadata.drop_all(bind=engine)
+
+    # drop_database(TEST_DATABASE_URL)
+
+
+@pytest.fixture
+def engine(db: Session) -> Session:
+    db.begin_nested()
+    yield db
+    db.rollback()
+
+
+@pytest.fixture
+def client(session):
+    app.dependency_overrides[get_db] = lambda: session
+    return TestClient(app)
