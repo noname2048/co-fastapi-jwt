a80c2ea6dd530a2e55007b10aadd64819e019748
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-02 22:36:16 +0900

    feat: add example in swagger docs

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,16 +1,16 @@
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field, SecretStr
 
 
 class User(BaseModel):
-    uuid: UUID
-    email: str
+    uuid: UUID = Field(..., example="123e4567-e89b-12d3-a456-426614174000")
+    email: EmailStr = Field(..., example="test@test.com")
 
     class Config:
         orm_mode = True
 
 
 class UserCreate(BaseModel):
-    email: str
-    password: str
+    email: EmailStr = Field(..., example="test@test.com")
+    password: SecretStr = Field(..., example="test1234")
