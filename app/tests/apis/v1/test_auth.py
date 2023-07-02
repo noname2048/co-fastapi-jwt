aafb5db1709dbf157813ba4246d51c31e6d14b64
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-03 00:02:22 +0900

    test: write auth verify test

--- app/tests/apis/v1/test_auth.py
+++ app/tests/apis/v1/test_auth.py
@@ -1,10 +1,11 @@
 from fastapi.testclient import TestClient
 
 
-def test_signup_and_login(client: TestClient) -> None:
+def test_auth(client: TestClient) -> None:
     fake_email = "test@test.com"
     fake_password = "test1234"
 
+    # test_signup_and_login
     response = client.post(
         "/api/v1/auth/signup",
         json={
@@ -27,3 +28,15 @@ def test_signup_and_login(client: TestClient) -> None:
     assert response.status_code == 200, response.text
     assert response.json()["access_token"]
     assert response.json()["refresh_token"]
+
+    access_token = response.json()["access_token"]
+    # refresh_token = response.json()["refresh_token"]
+
+    # test_verify_and_refresh
+    response = client.post(
+        "api/v1/auth/verify",
+        json={
+            "access_token": access_token,
+        },
+    )
+    assert response.status_code == 200, response.text
