29a095fbc7dda8a373b29fe89ea6caa69b5d791a
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-02 23:38:06 +0900

    test: add signup_and_login test

--- app/tests/apis/v1/test_auth.py
+++ app/tests/apis/v1/test_auth.py
@@ -1,18 +1,29 @@
-from fastapi import status
 from fastapi.testclient import TestClient
 
 
-def test_signup_and_signin(client: TestClient) -> None:
+def test_signup_and_login(client: TestClient) -> None:
     fake_email = "test@test.com"
     fake_password = "test1234"
 
     response = client.post(
-        "/api/v1/token/login",
-        data={
+        "/api/v1/auth/signup",
+        json={
             "email": fake_email,
             "password": fake_password,
         },
     )
 
-    assert response.status_code == status.HTTP_201_CREATED, response.status_code
-    assert response.json()["uuid"].len() == 36
+    assert response.status_code == 200, response.text
+    assert len(response.json()["uuid"]) == 36
+
+    response = client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": fake_email,
+            "password": fake_password,
+        },
+    )
+
+    assert response.status_code == 200, response.text
+    assert response.json()["access_token"]
+    assert response.json()["refresh_token"]
