3665fd45cb101623041933eaa7edf43dee1b4acd
Sungwook Choi <sungwook.csw@gmail.com>
2023-07-02 23:37:52 +0900

    feat: change tag jwt to auth

--- app/apis/v1/api.py
+++ app/apis/v1/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from .jwt import router as jwt_router
+from .auth import router as auth_router
 from .user import router as user_router
 
 api_router = APIRouter()
 api_router.include_router(user_router, tags=["user"])
-api_router.include_router(jwt_router, tags=["jwt"])
+api_router.include_router(auth_router, tags=["auth"])
